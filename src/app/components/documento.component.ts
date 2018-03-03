cb92f931b6250f80e9980371e66f4db93ef623aa
i52gagrj <i52gagrj@gmail.com>
2018-03-03 23:05:23 +0100

    201803032305

--- src/app/components/documento.component.ts
+++ src/app/components/documento.component.ts
@@ -1,31 +1,33 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, EventEmitter, NgZone, Inject} from '@angular/core';
+import { Http, Response } from '@angular/http';
 import {Router, ActivatedRoute, Params} from '@angular/router';
-//import { UserService } from '../services/user.service';
+// import {RutasDeArchivosService} from '../rutas-de-archivos.service';
+import { NGUploaderOptions, UploadedFile, UploadRejected } from 'ngx-uploader';
+// import { UserService } from '../services/user.service';
 
 @Component({
 	selector: 'documento',
 	templateUrl: '../views/documento.html'/*,
 	providers: [UserService]*/
 })
-export class DocumentoComponent implements OnInit{
+export class DocumentoComponent implements OnInit {
 	public title: string;
-	//public user;
-	//public identity;
-	//public token;
+	// public user;
+	// public identity;
+	// public token;
 
 	constructor(
 		private _route: ActivatedRoute,
 		private _router: Router,
-		//private _userService: UserService
-	){
+		// private _userService: UserService
+	) {
 		this.title = 'Documentos';
 
 	}
 
-	ngOnInit(){
+	ngOnInit() {
 		console.log('El componente documento.component ha sido cargado!!');
 		console.log('A por el bote!!!');
 
 	}
-
-}	
+}
